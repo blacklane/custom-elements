6dd2334987f359f2e9e447026c9c7d7f48032768
Daniel Olano <danielolanoc@gmail.com>
2017-05-15 12:26:12 +0200

    Include template loader

---
+++ templates.js
@@ -0,0 +1,24 @@
+const templates = Object.create(null, {
+  load: {
+    value: async function(fileName) {
+      const url = new URL(fileName,
+        document.currentScript && document.currentScript.src || location.href)
+      if (url in this) return this[url]
+      // fetch and parse template as string
+      let template = await fetch(url)
+      template = await template.text()
+      template = new DOMParser().parseFromString(template, 'text/html')
+        .querySelector('template')
+      if (!template) throw new TypeError('No template element found')
+      // overwrite link tags' hrefs asuming they're always relative to the template
+      for (let link of template.content.querySelectorAll('link')) {
+        let href = document.importNode(link).href
+        href = new URL(href).pathname.substr(1)
+        link.href = new URL(href, url).href
+      }
+      document.head.append(template)
+      this[url] = template
+      return template
+    }
+  }
+})
